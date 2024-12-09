c92ccdd2d10f34d658d5a26a8c406615c3ba74e1
Dhanush J <dhanushkumarbeeee@gmail.com>
2024-12-09 12:35:37 +0530

    Add project files.

---
+++ JSON Serialization and Deserialization in C#/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace JSON_Serialization_and_Deserialization_in_C_
+{
+    [Serializable]
+    public class Fruit
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create an instance of the class
+            Fruit fruit = new Fruit { Name = "Apple", Price = 26 };
+            // Serialize the object to JSON
+            string json = SerializeObjectToJson(fruit);
+            // Display the serialized JSON
+            Console.WriteLine("Serialized JSON:");
+            Console.WriteLine(json);
+            // Deserialize the JSON to an object
+            Fruit deserializedPerson = DeserializeJsonToObject(json);
+            // Display the deserialized object
+            Console.WriteLine("Deserialized Object:");
+            Console.WriteLine($"Name: {deserializedPerson.Name}, Price: {deserializedPerson.Price}");
+        }
+        // Serialize an object to JSON
+        static string SerializeObjectToJson(Fruit obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+        // Deserialize JSON to an object
+        static Fruit DeserializeJsonToObject(string json)
+        {
+            return JsonSerializer.Deserialize<Fruit>(json);
+        }
+    }
+}
